471ddf42125a3f65b2b6d6ac118447f5c94dd50e
Mohammed <mohammed@mofayedh.com>
2022-07-07 20:54:24 +0300

    Add Done page when submiting

---
+++ done.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+     rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+       crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/main.css">
+    <title>HHC FORM SAMPLE</title>
+</head>
+<body>
+    <header>
+        <div class="HHC-logo"><a href="#"><img src="assets/imgs/HHC-Logo.png" alt="HHC LOGO"></a></div>
+        <nav>
+            <ul class="nav-items">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        
+    <div class="done-msg">
+        <h1>Thanks, Your REQUEST has been deliverd</h1>
+        <img src="assets/imgs/checked.png" alt="">
+    </div>
+        
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
