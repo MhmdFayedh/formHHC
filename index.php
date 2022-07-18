8c7c623ad2c8bb5ee1e17be98d86340236711f42
Mohammed <mohammed@mofayedh.com>
2022-07-18 07:58:38 +0300

    Modifiy A word (:

--- index.php
+++ index.php
@@ -132,7 +132,7 @@ include_once "functions/uploader.php";
         
     </main>
         <footer>
-            <p>All rights reseved to Hail Health Cluster&copy;</p>
+            <p>All rights reserved to Hail Health Cluster&copy;</p>
         </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
